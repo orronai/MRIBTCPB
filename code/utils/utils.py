3c0df7032d2aeb0721c28605def631c29b9bc189
Or Ronai <orronai@gmail.com>
2023-06-23 22:08:02 +0300

    add t-sne implementation of the classifier encoder

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -1,7 +1,15 @@
 import torch
 import numpy as np
 import matplotlib
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from sklearn.manifold import TSNE
+
+from MRIBTCPB.code.utils.datasets import (
+    CLASSES, IMAGE_SIZE, NUM_WORKERS, TEST_DIR, get_transform,
+)
 
 matplotlib.style.use('ggplot')
 
@@ -75,3 +83,50 @@ def calculate_accuracy(model, dataloader, device):
 
     model_accuracy = total_correct / total_images * 100
     return model_accuracy, confusion_matrix
+
+def plot_tsne_representation(classifier, n_points=500, n_components=2, perplexity=30.0):
+    if n_components < 2 or n_components > 3:
+        print("OH NO :(")
+        raise SystemError("2 <= dim <= 3")
+
+    device = ('cuda' if torch.cuda.is_available() else 'cpu')
+    dataset_test = datasets.ImageFolder(
+        TEST_DIR, transform=(get_transform(IMAGE_SIZE)),
+    )
+
+    test_loader = DataLoader(
+        dataset_test, batch_size=n_points,
+        shuffle=True, num_workers=NUM_WORKERS,
+    )
+    n_neighbors = len(CLASSES)
+
+    with torch.no_grad():
+        X, labels = next(iter(test_loader))
+        latent_X = classifier.get_representation(X.to(device)).view(n_points, -1).data.cpu().numpy()
+        labels = labels.data.cpu().numpy()
+
+    fig = plt.figure(figsize=(8, 8))
+    x_tsne = TSNE(n_components, perplexity=perplexity).fit_transform(latent_X)
+
+    if n_components == 2:
+        ax = fig.add_subplot(1, 1, 1)
+        for l in range(n_neighbors):
+            ax.scatter(
+                x_tsne[labels == l, 0], x_tsne[labels == l, 1], color=colors[l],
+                marker='.', label=f'class={CLASSES[l]}',
+            )
+        ax.set_title("2D t-SNE of the embedded images")
+    else:
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+        colors = cm.rainbow(np.linspace(0, 1, n_neighbors))
+        for l in range(n_neighbors):
+            ax.scatter(
+                x_tsne[labels == l, 0], x_tsne[labels == l, 1], x_tsne[labels == l, 2],
+                color=colors[l], marker='.', label=f'class={CLASSES[l]}',
+            )
+        ax.set_title("3D t-SNE of the embedded images")
+
+    ax.grid()
+    ax.legend(bbox_to_anchor=(1, 1), loc="upper left")
+    plt.tight_layout()
+    plt.savefig(f"MRIBTCPB/outputs/'t-SNE_BYOL-{n_components}D'.png")
